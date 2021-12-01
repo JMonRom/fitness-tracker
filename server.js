12eb52ca1798f116853397c886edb9cee464d7e0
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-12-01 01:21:24 -0800

    fixing add exercise function

--- server.js
+++ server.js
@@ -1,21 +1,89 @@
 const express = require('express')
-const logger = require('morgan');
 const mongoose = require('mongoose');
-
-const PORT = process.env.PORT || 3009;
-
+const path = require('path');
+const Workout = require('./schema')
 const app = express();
+const PORT = process.env.PORT || 3009;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
-app.use(express.static('public'));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// teammates Kevin, Daniel and Jericho helped work on setting the routes up as well
+// inserting routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-// include API routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+app.get("/api/workouts", async (req, res) => {
+  try {
+    const workout = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ]);
+
+    res.json(workout);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+app.get("/api/workouts/range", async (req, res) => {
+  try {
+    const workout = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ]).sort({day: -1})
+
+    res.json(workout);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+app.post("/api/workouts", (req, res) => {
+  Workout.create(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => res.json(err));
+});
+
+app.post("/api/workouts/:id", (req, res) => {
+  Workout.updateOne(
+    { _id: req.params.id },
+    {
+      $push: { exercises: req.body },
+    }
+  ).then((dbWorkout) => {
+    res.json(dbWorkout);
+  }).catch((err) => res.json(err));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening: http://localhost:${PORT}`);
